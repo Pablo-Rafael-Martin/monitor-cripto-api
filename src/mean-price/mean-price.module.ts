d249696270acd94c6162b9ac83addb3c57d51518
Pablo Rafael Caramori Fontes Martin <pablo.rafael.cfm@gmail.com>
2025-06-09 20:47:45 -0300

    Recomeçando do zero

--- src/mean-price/mean-price.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MeanPriceService } from './mean-price.service';
-import { MeanPriceController } from './mean-price.controller';
-
-@Module({
-  providers: [MeanPriceService],
-  controllers: [MeanPriceController]
-})
-export class MeanPriceModule {}
